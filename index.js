b247fece74904eaf8e39bdf485620e3691a66042
Thabang Rammitlwa <thabang451@gmail.com>
2024-03-21 09:42:59 +0200

    Flavor Selection

--- index.js
+++ index.js
@@ -4,8 +4,16 @@
 // Given a variable that holds a user's preferred type of Monster Energy drink, 
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
-let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+let userPreference = "Sugar-free"; // Possible values: 'Regular', 'Sugar-free', or undefined
+if (userPreference==="regular"){
+    console.log("User prefers Regular Monster Energy drink");
+}
+else if(userPreference==='sugar-free'){
+    console.log("User prefers Sugar-free Monster Energy drink.");
+}
+else{
+    console.log(`User preference is not set,defaulting to Regular Monster Energy drink.`);
+}
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
